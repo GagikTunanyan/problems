4bc22d4c713341471578a319b0245e5dd49a0e86
Gagik Tunanyan <gagiktunanyan@gmail.com>
2020-06-03 17:20:13 +0400

    xndir1 anverj cikl 7akan hamakargi nerkayacum

--- problems.js
+++ problems.js
@@ -1,7 +1,23 @@
 //trvac tivy nerkayacnel 7 akan hamakargov
 
-
 let func =(nums) =>{
-  return nums.toString(7)
+    let s = "";
+    let nshan ="";
+    let arjeq = nums;
+    if (arjeq < 0) {
+        arjeq *= -1;
+        nshan = "-"
+    }
+    while (true) {
+        if (arjeq < 7) {
+            s = arjeq + s;
+            break;
+        }
+        s = (arjeq % 7) + s;
+        arjeq = parseInt(arjeq / 7);
+    }
+
+    return nshan + s
 };
-console.log(func(7));
+
+console.log(func(-15));
