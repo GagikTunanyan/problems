2410d6de764b35a1834b3c4c58acc53f64ba8d01
Gagik Tunanyan <gagiktunanyan@gmail.com>
2020-06-04 19:26:00 +0400

    fibonachi recursive tarberak

---
+++ fibonachi.js
@@ -0,0 +1,11 @@
+//fibonaci tveri sharq,trvac tivy havasar e naxkin 2 fibonachi tveri gumarin
+// payman fib(n) = fib(n-1)+fib(n-2) if n < 3 return 1
+
+let fibonachi = (num) => {
+  if (num < 3) {
+      return 1;
+  }
+
+  return fibonachi(num-1) + fibonachi(num-2);
+};
+console.log(fibonachi(6));
