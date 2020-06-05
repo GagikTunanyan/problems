874e7b766715742ea8a8e1d0fc422b4c88c754a0
Gagik Tunanyan <gagiktunanyan@gmail.com>
2020-06-05 15:04:40 +0400

    compare_the_triplets karj iterativ

--- Compare_the_Triplets.js
+++ Compare_the_Triplets.js
@@ -2,45 +2,65 @@
 //arandzin masivnerum, a ev b , petq e veradarcnenq masiv, vortex grvac e hamematutyuny hashivi kargov
 //orinak a = [2,5,3,1] b = [2,3,5,7,9,6,1] return [1,5]
 
-let compare_the_triplets = (a,b) =>{
+// let compare_the_triplets = (a,b) =>{
+//   let hashiv = [0,0];
+//   if (a.length >= b.length){
+//       for (let i = 0; i < a.length; i++) {
+//           if (b[i] !== undefined) {
+//               if (a[i] === b[i]) {
+//                   i++
+//               }
+//               if (a[i] > b[i]) {
+//                   hashiv[0]++
+//               }
+//               else {
+//                   hashiv[1]++
+//               }
+//           }
+//           else {
+//               hashiv[0]++
+//           }
+//       }
+//   }
+//   else {
+//       for (let i = 0; i < b.length; i++){
+//           if (a[i] !== undefined) {
+//               if (a[i] === b[i]) {
+//                   i++
+//               }
+//               if (a[i] > b[i]) {
+//                   hashiv[0]++
+//               }
+//               else {
+//                   hashiv[1]++
+//               }
+//           }
+//           else {
+//              hashiv[1]++
+//           }
+//       }
+//   }
+//   return hashiv
+// };
+
+let compare_the_triplets = (a,b) => {
   let hashiv = [0,0];
-  if (a.length >= b.length){
-      for (let i = 0; i < a.length; i++) {
-          if (b[i] !== undefined) {
-              if (a[i] === b[i]) {
-                  i++
-              }
-              if (a[i] > b[i]) {
-                  hashiv[0]++
-              }
-              else {
-                  hashiv[1]++
-              }
-          }
-          else {
-              hashiv[0]++
+  for (let i = 0; i < a.length; i++){
+      if (a[i] === b[i]){
+          i++;
+      }
+      if (a[i] > b[i]){
+          hashiv[0]++
+      }
+      else {
+          if (a[i] !== b[i]) {
+              hashiv[1]++
           }
       }
   }
-  else {
-      for (let i = 0; i < b.length; i++){
-          if (a[i] !== undefined) {
-              if (a[i] === b[i]) {
-                  i++
-              }
-              if (a[i] > b[i]) {
-                  hashiv[0]++
-              }
-              else {
-                  hashiv[1]++
-              }
-          }
-          else {
-             hashiv[1]++
-          }
-      }
-  }
+
   return hashiv
+
 };
 
 console.log(compare_the_triplets([20,20,30],[20,20,50]));
